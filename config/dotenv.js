76a4f9c419fae01a0941a11b2e7c0dd03cf67f99
Bernardo Gurgel <brnrdog@hey.com>
2022-02-03 18:08:22 +0100

    Fix styles ordering and environment variable name for gh token

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: ['GITHUB_TOKEN'],
+    clientAllowedKeys: ['GITHUB_API_TOKEN'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env'),
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,8 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 const cssModules = {
+  headerModules: ['github-profile/styles/app'],
+
   plugins: [
     require('postcss-import')({ path: ['node_modules'] }),
     require('tailwindcss')('./tailwind.config.js'),
