024cae37ba815184e29ba72d28041ce6e5579a5f
Bernardo Gurgel <brnrdog@hey.com>
2022-02-09 22:51:06 +0100

    Adjust loading states and add missing ember-date-fns

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 const cssModules = {
   headerModules: ['github-profile/styles/app'],
-
+  options: {
+    postcss: {
+      cacheInclude: [/.*\.(css|scss|hbs)$/, /.tailwind\/config\.js$/],
+    },
+  },
   plugins: [
     require('postcss-import')({ path: ['node_modules'] }),
     require('tailwindcss')('./tailwind.config.js'),
